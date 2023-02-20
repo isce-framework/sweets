09b138e1f6e8318707adc005768aed24a9ccdc49
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2023-02-20 01:12:11 -0500

    add first version of dask running tasks

---
+++ src/sweets/core.py
@@ -0,0 +1,125 @@
+from datetime import date, datetime
+
+# from os import fspath
+from pathlib import Path
+from typing import Any, List, Optional
+
+from dask.distributed import Client
+from dateutil.parser import parse
+from pydantic import BaseModel, Extra, Field, PrivateAttr, validator
+
+from ._burst_db import download_burst_db
+from ._log import get_log, log_runtime
+from ._orbits import download_orbits
+from .dem import DEM
+from .download import ASFQuery
+
+logger = get_log()
+
+
+class Workflow(BaseModel):
+    """Class for end-to-end processing of Sentinel-1 data."""
+
+    # Steps
+    bbox: tuple = Field(
+        ...,
+        description=(
+            "lower left lon, lat, upper right format e.g."
+            " bbox=(-150.2,65.0,-150.1,65.5)"
+        ),
+    )
+    start: datetime = Field(
+        None,
+        description=(
+            "Starting time for search. Many acceptable inputs e.g. '3 months and a day"
+            " ago' 'May 30, 2018' '2010-10-30T00:00:00Z'"
+        ),
+    )
+    end: datetime = Field(
+        None,
+        description=(
+            "Ending time for search. Many acceptable inputs e.g. '3 months and a day"
+            " ago' 'May 30, 2018' '2010-10-30T00:00:00Z'"
+        ),
+    )
+    track: Optional[int] = Field(
+        None,
+        description="Path number",
+    )
+    dem: DEM = Field(
+        None,
+        description="DEM parameters.",
+    )
+    asf_query: ASFQuery = Field(
+        None,
+        description="ASF query parameters.",
+    )
+    orbit_dir: Path = Field(
+        Path("orbits"),
+        description="Directory for orbit files.",
+    )
+    n_workers: int = Field(
+        4,
+        description="Number of workers to use for processing.",
+    )
+    _client: Client = PrivateAttr()
+
+    class Config:
+        extra = Extra.forbid  # raise error if extra fields passed in
+
+    @validator("start", "end", pre=True)
+    def _parse_date(cls, v):
+        if isinstance(v, datetime):
+            return v
+        elif isinstance(v, date):
+            # Convert to datetime
+            return datetime.combine(v, datetime.min.time())
+        return parse(v)
+
+    @validator("dem", pre=True, always=True)
+    def _use_same_bbox(cls, v, values):
+        if v is not None:
+            return v
+        bbox = values.get("bbox")
+        return DEM(bbox=bbox)
+
+    @validator("asf_query", pre=True, always=True)
+    def _create_query(cls, v, values):
+        if v is not None:
+            return v
+        bbox = values.get("bbox")
+        return ASFQuery(
+            bbox=bbox,
+            start=values.get("start"),
+            end=values.get("end"),
+            relativeOrbit=values.get("track"),
+        )
+
+    def __init__(self, **data: Any) -> None:
+        super().__init__(**data)
+        # Scale later
+        logger.info("Starting Dask cluster")
+        self._client = Client(n_workers=1, threads_per_worker=1)
+
+    @log_runtime
+    def run(self) -> List[str]:
+        """Run the workflow."""
+        # start our Dask cluster
+
+        logger.info(f"Scaling dask cluster to {self.n_workers} workers")
+        self._client.cluster.scale(self.n_workers)
+
+        # TODO: background processing, maybe with dask or prefect
+        # https://examples.dask.org/applications/prefect-etl.html
+        # downloaded_files = self.asf_query.download_data()
+        downloaded_files = self._client.submit(self.asf_query.download)
+        slc_data_path = self.asf_query.out_dir
+
+        self._client.submit(download_orbits, slc_data_path, self.orbit_dir)
+
+        dem_file = self._client.submit(self.dem.create)
+        burst_db_file = self._client.submit(download_burst_db)
+
+        # Wait until all the files are downloaded
+        # downloaded_files, dem_file, burst_db_file = self._client.gather(
+        return self._client.gather([downloaded_files, dem_file, burst_db_file])
