79025903a9450e2a08d5d21fae1abdbd5b23c8c5
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2023-08-11 10:10:02 -0700

    add start of tests for missing data

---
+++ tests/test_missing_data.py
@@ -0,0 +1,57 @@
+from pathlib import Path
+
+import numpy as np
+import pytest
+from osgeo import gdal
+
+from sweets._missing_data import Stats, get_bad_files, get_raster_stats, is_valid
+
+
+# Fixture for creating temporary files
+@pytest.fixture()
+def slc_file_list(tmp_path):
+    shape = (10, 100, 100)
+    nodata = np.nan  # Set nodata value
+
+    # Create random data with a certain percentage of nodata pixels
+    slc_stack = np.random.random(shape)
+    nodata_mask = np.random.choice(
+        [True, False], shape, p=[0.2, 0.8]
+    )  # 20% of pixels are nodata
+    slc_stack[nodata_mask] = nodata
+
+    # Write to a file
+    driver = gdal.GetDriverByName("GTiff")
+    start_date = 20220101
+    d = tmp_path / "gtiffs"
+    d.mkdir()
+    name_template = d / "{date}.slc.tif"
+    file_list = []
+    for i in range(shape[0]):
+        fname = str(name_template).format(date=str(start_date + i))
+        file_list.append(Path(fname))
+        ds = driver.Create(fname, shape[-1], shape[-2], 1, gdal.GDT_CFloat32)
+        ds.GetRasterBand(1).WriteArray(slc_stack[i])
+        ds.GetRasterBand(1).SetNoDataValue(nodata)
+        ds = None
+
+    return file_list
+
+
+# Now we write test cases using the created fixture
+def test_get_bad_files(slc_file_list):
+    bad_files, bad_stats = get_bad_files(str(slc_file_list[0].parent), max_jobs=1)
+    assert isinstance(bad_files, list)
+    assert isinstance(bad_stats, list)
+    get_bad_files(str(slc_file_list[0].parent), max_jobs=5)
+
+
+def test_is_valid(slc_file_list):
+    valid, reason = is_valid(str(slc_file_list[0]))
+    assert valid
+    assert reason == ""
+
+
+def test_get_raster_stats(slc_file_list):
+    stats = get_raster_stats(str(slc_file_list[0]))
+    assert isinstance(stats, Stats)
