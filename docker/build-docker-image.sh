1e5d8cd597dd5c0c716feaa0111a7d0edeb97349
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2023-08-11 16:57:18 -0700

    add dockerfile and scripts

---
+++ docker/build-docker-image.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+TAG=latest
+
+# Enable common error handling options.
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Parse input arguments
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+    -t | --tag)
+        TAG="$2"
+        shift
+        shift
+        ;;
+        # Add the ability to specify build-arg MAMBA_USER_ID
+    -u | --user-id)
+        MAMBA_USER_ID="$2"
+        shift
+        shift
+        ;;
+        # Add ability to specify base image
+    -b | --base)
+        BASE="$2"
+        shift
+        shift
+        ;;
+    *)
+        echo "Unknown option: $key"
+        exit 1
+        ;;
+    esac
+done
+
+# Use 'latest' as the default tag if not specified
+if [ -z "$TAG" ]; then
+    TAG="latest"
+fi
+
+# Build the Docker image
+CMD_BASE="docker build --network=host --tag isce-framework/sweets:$TAG --file docker/Dockerfile"
+
+# append --build-arg if specified:
+if [ -z "${BASE+x}" ]; then
+    CMD_BASE="$CMD_BASE"
+else
+    CMD_BASE="$CMD_BASE --build-arg BASE=$BASE"
+fi
+
+# append MAMBA_USER_ID if specified:
+if [ -z "${MAMBA_USER_ID+x}" ]; then
+    CMD_BASE="$CMD_BASE"
+else
+    CMD_BASE="$CMD_BASE --build-arg MAMBA_USER_ID=$MAMBA_USER_ID"
+fi
+
+# finish with ".":
+CMD_BASE="$CMD_BASE ."
+echo $CMD_BASE
+# Run the command
+eval $CMD_BASE
+
+# To run the image and see the help message....
+echo "To run the image and see the help message:"
+echo "docker run --rm -it isce-framework/sweets:$TAG sweets --help"
+#
+echo "To run the workflow on a configuration in the current directory...."
+echo "docker run --user \$(id -u):\$(id -g) -v \$PWD:/work --rm -it isce-framework/sweets:$TAG sweets run sweets_config.yaml"
+# where...
+#     --user $(id -u):$(id -g)  # Needed to avoid permission issues when writing to the mounted volume.
+#     -v $PWD:/work  # Mounts the current directory to the /work directory in the container.
+#     --rm  # Removes the container after it exits.
+#     -it  # Needed to keep the container running after the command exits.
+#     isce-framework/sweets:latest  # The name of the image to run.
+#     sweets run sweets_config.yaml # The `sweets` command that is run in the container
