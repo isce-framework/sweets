6ee8de6332b92a0475e38efc7afea7e8b56f87c0
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2023-10-03 18:51:22 -0700

    add more flexible `browse_arrays`, more options to `browse_ifgs`

--- src/sweets/plotting.py
+++ src/sweets/plotting.py
@@ -1,12 +1,13 @@
 from __future__ import annotations
 
 from pathlib import Path
-from typing import List, Optional, Tuple, Union
+from typing import Optional, Sequence, Tuple, Union
 
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import geopandas as gpd
 import ipywidgets
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
 import numpy as np
@@ -14,6 +15,7 @@ from cartopy.io import shapereader
 from cartopy.mpl.gridliner import LATITUDE_FORMATTER, LONGITUDE_FORMATTER
 from dolphin import io
 from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.image import AxesImage
 from numpy.typing import ArrayLike
 from shapely.geometry import Polygon, box
 
@@ -95,15 +97,39 @@ def plot_ifg(
     return fig, ax
 
 
+def _get_unique_cm(arr: np.ndarray, name: str = "jet"):
+    """Get a colormap with unique colors for each value in arr."""
+    return mpl.cm.get_cmap(name, len(np.unique(arr)))
+
+
+def _plot_cc(
+    ccl: np.ndarray,
+    ax: plt.Axes,
+    title: str = "",
+    cmap: str = "jet",
+    do_colorbar: bool = True,
+) -> AxesImage:
+    """Plot a connected component image with unique colors."""
+    axim = ax.imshow(ccl, cmap=_get_unique_cm(ccl, name=cmap), interpolation="nearest")
+    fig = ax.figure
+    ax.set_title(title)
+    if do_colorbar:
+        fig.colorbar(axim, ax=ax, ticks=np.arange(np.min(ccl), np.max(ccl) + 1))
+    return axim
+
+
 def browse_ifgs(
     sweets_path: Optional[Filename] = None,
-    file_list: Optional[list[Filename]] = None,
+    file_list: Optional[Sequence[Filename]] = None,
+    cor_list: Optional[Sequence[Filename]] = None,
+    unw_list: Optional[Sequence[Filename]] = None,
+    conncomp_list: Optional[Sequence[Filename]] = None,
     amp_image: Optional[ArrayLike] = None,
     figsize: tuple[int, int] = (7, 4),
     vm_unw: float = 10,
     vm_cor: float = 1,
     unw_suffix: str = UNW_SUFFIX,
-    layout="horizontal",
+    layout="box",
     axes: Optional[plt.Axes] = None,
     ref_unw: Optional[tuple[float, float]] = None,
     subsample_factor: Union[int, tuple[int, int]] = 1,
@@ -118,6 +144,15 @@ def browse_ifgs(
         Path to sweets directory.
     file_list : list[Filename], optional
         Alternative to `sweets_path`, directly provide a list of ifg files.
+    cor_list : list[Filename], optional
+        List of correlation files, if the interferograms in `file_list` are not
+        normalized such that `np.abs(ifg)` is the correlation.
+    unw_list : list[Filename], optional
+        List of unwrapped interferogram files, if providing the
+        interferograms/correlation files via `file_list`.
+    conncomp_list : list[Filename], optional
+        List of unwrapped connected component label files, if providing
+        the interferograms/correlation files via `file_list`.
     amp_image : ArrayLike, optional
         If provided, plots an amplitude image along with the ifgs for visual reference.
     figsize : tuple
@@ -128,8 +163,8 @@ def browse_ifgs(
         Value used as min/max cutoff for correlation phase plot.
     unw_suffix : str, default = ".unw.tif"
         Suffix to use to search for unwrapped phase images.
-    layout : str
-        Layout of the plot. Can be "horizontal" or "vertical".
+    layout : str, default="box"
+        Layout of the plot. Can be "box, "horizontal" or "vertical".
     axes : matplotlib.pyplot.Axes
         If provided, use this array of axes to plot the images.
         Otherwise, creates a new figure.
@@ -158,32 +193,48 @@ def browse_ifgs(
     num_panels = 2  # ifg, cor
 
     # Check if we have unwrapped images
-    unw_list = [
-        Path(str(i).replace("stitched", "unwrapped")).with_suffix(unw_suffix)
-        for i in file_list
-    ]
-    num_existing_unws = sum(f.exists() for f in unw_list)
+    if unw_list is None:
+        unw_list = [
+            Path(str(i).replace("stitched", "unwrapped")).with_suffix(unw_suffix)
+            for i in file_list
+        ]
+    num_existing_unws = sum(Path(f).exists() for f in unw_list)
     if num_existing_unws > 0:
         print(f"Found {num_existing_unws} {unw_suffix} files")
+        num_panels += 1
+
+    conncomp_suffix = ".unw.conncomp"
+    if conncomp_list is None:
+        conncomp_list = [Path(p).with_suffix(conncomp_suffix) for p in file_list]
+    num_existing_conncomps = sum(Path(f).exists() for f in conncomp_list)
+    if num_existing_conncomps > 0:
+        print(f"Found {num_existing_conncomps} {conncomp_suffix} files")
         num_panels += 1
 
     if amp_image is not None:
         num_panels += 1
 
     if axes is None:
-        subplots_dict = dict(figsize=figsize, sharex=True, sharey=True)
-        if layout == "horizontal":
+        subplots_dict = dict(figsize=figsize, sharex=True, sharey=True, squeeze=False)
+        if layout == "box":
+            subplots_dict["nrows"] = int(np.ceil(np.sqrt(num_panels)))
+            subplots_dict["ncols"] = int(np.ceil(np.sqrt(num_panels)))
+        elif layout == "horizontal":
             subplots_dict["ncols"] = num_panels
         else:
             subplots_dict["nrows"] = num_panels
         fig, axes = plt.subplots(**subplots_dict)
+        axes = axes.ravel()
     else:
         fig = axes[0].figure
 
     # imgs = np.stack([io.load_gdal(f) for f in file_list])
     img = io.load_gdal(file_list[0], subsample_factor=subsample_factor)
     phase = np.angle(img)
-    cor = np.abs(img)
+    if cor_list is not None:
+        cor = io.load_gdal(cor_list[0], subsample_factor=subsample_factor)
+    else:
+        cor = np.abs(img)
     titles = [f.stem for f in file_list]  # type: ignore
 
     # plot once with colorbar
@@ -194,10 +245,13 @@ def browse_ifgs(
     axim_cor = axes[1].imshow(cor, cmap="plasma", vmin=0, vmax=vm_cor)
     fig.colorbar(axim_cor, ax=axes[1])
 
-    if unw_list[0].exists():
+    if Path(unw_list[0]).exists():
         unw = apply_ref(io.load_gdal(unw_list[0], subsample_factor=subsample_factor))
         axim_unw = axes[2].imshow(unw, cmap="RdBu", vmin=-vm_unw, vmax=vm_unw)
         fig.colorbar(axim_unw, ax=axes[2])
+    if Path(conncomp_list[0]).exists():
+        ccl = io.load_gdal(conncomp_list[0], subsample_factor=subsample_factor)
+        axim_ccl = _plot_cc(ccl, ax=axes[3], cmap="jet")
 
     if amp_image is not None:
         vm_amp = np.nanpercentile(amp_image.ravel(), 99)
@@ -206,10 +260,12 @@ def browse_ifgs(
 
     @ipywidgets.interact(idx=(0, len(file_list) - 1))
     def browse_plot(idx=0):
-        phase = np.angle(
-            io.load_gdal(file_list[idx], subsample_factor=subsample_factor)
-        )
-        cor = np.abs(io.load_gdal(file_list[idx], subsample_factor=subsample_factor))
+        ifg = io.load_gdal(file_list[idx], subsample_factor=subsample_factor)
+        phase = np.angle(ifg)
+        if cor_list is not None:
+            cor = io.load_gdal(cor_list[idx], subsample_factor=subsample_factor)
+        else:
+            cor = np.abs(ifg)
         axim_ifg.set_data(phase)
         axim_cor.set_data(cor)
         if unw_list[idx].exists():
@@ -217,7 +273,91 @@ def browse_ifgs(
                 io.load_gdal(unw_list[idx], subsample_factor=subsample_factor)
             )
             axim_unw.set_data(unw)
+        if conncomp_list[idx].exists():
+            ccl = io.load_gdal(conncomp_list[idx], subsample_factor=subsample_factor)
+            axim_ccl.set_data(ccl)
         fig.suptitle(titles[idx])
+
+
+def browse_arrays(
+    img_stack: np.ndarray,
+    cmap: str | None = None,
+    titles: Sequence[str] | None = None,
+    axes: Optional[plt.Axes] = None,
+    figsize=(6, 6),
+    **subplots_dict,
+):
+    """Browse a stack of images interactively.
+
+    Like `browse_ifgs`, but for any pre-loaded numpy array.
+
+    Parameters
+    ----------
+    img_stack : np.ndarray
+        3D numpy array holding images.
+    cmap : str
+        colormap to plot `img_stack` with
+    titles : Sequence[str], optional
+        Title to use for each image. Length should match `len(img_stack)`
+    amp_image : ArrayLike, optional
+        If provided, plots an amplitude image along with the ifgs for visual reference.
+    figsize : tuple
+        Figure size.
+    vm_unw : float
+        Value used as min/max cutoff for unwrapped phase plot.
+    vm_cor : float
+        Value used as min/max cutoff for correlation phase plot.
+    unw_suffix : str, default = ".unw.tif"
+        Suffix to use to search for unwrapped phase images.
+    layout : str, default="box"
+        Layout of the plot. Can be "box, "horizontal" or "vertical".
+    axes : matplotlib.pyplot.Axes
+        If provided, use this array of axes to plot the images.
+        Otherwise, creates a new figure.
+    ref_unw : Optional[tuple[int, int]]
+        Reference point for all .unw files.
+        If not passed, subtracts the mean of each file.
+    subsample_factor : int or tuple[int, int]
+        Amount to downsample when loading images.
+    """
+    num_panels = 2 if np.iscomplexobj(img_stack) else 1
+    if axes is None:
+        subplots_dict = dict(figsize=figsize, squeeze=False, sharex=True, sharey=True)
+        subplots_dict["ncols"] = num_panels
+        fig, axes = plt.subplots(**subplots_dict)
+        img = np.angle(img_stack[0])
+        amp = np.abs(img_stack[0])
+    else:
+        fig = axes[0].figure
+        img = img_stack[0]
+        amp = None
+
+    axes = axes.ravel()
+
+    if num_panels == 2:
+        # plot once with colorbar
+        axim_img = axes[0].imshow(img, cmap="dismph", vmin=-3.14, vmax=3.14)
+        amp_vmax = np.percentile(np.abs(img_stack), 99)
+        axim_amp = axes[1].imshow(amp, cmap=cmap, vmax=amp_vmax)
+
+        fig.colorbar(axim_img, ax=axes[0])
+        fig.colorbar(axim_amp, ax=axes[1])
+    else:
+        axim_img = axes[0].imshow(img, cmap=cmap)
+        axim_amp = None
+
+    @ipywidgets.interact(idx=(0, len(img_stack) - 1))
+    def browse_plot(idx=0):
+        if np.iscomplexobj(img_stack):
+            phase = np.angle(img_stack[idx])
+            axim_img.set_data(phase)
+            amp = np.abs(img_stack[idx])
+            axim_amp.set_data(amp)
+        else:
+            axim_img.set_data(img_stack[idx])
+
+        if titles:
+            fig.suptitle(titles[idx])
 
 
 def _make_dismph_colors():
@@ -253,7 +393,7 @@ except:
 def plot_area_of_interest(
     state: Optional[str] = None,
     bbox: Optional[Tuple[float, float, float, float]] = None,
-    area_coordinates: Optional[List[Tuple[float, float]]] = None,
+    area_coordinates: Optional[Sequence[Tuple[float, float]]] = None,
     buffer: float = 0.0,
     grid_step: Optional[float] = 1.0,
     ax=None,
